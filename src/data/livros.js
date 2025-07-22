39c5b22c482274bb1ee3c509dad1d2b837839625
davittiw <lucctyte@gmail.com>
2025-07-22 15:41:55 -0300

    error images search

--- src/data/livros.js
+++ src/data/livros.js
@@ -5,7 +5,7 @@ const livros = [
       autor: "Stephen King",
       sinopse: "Jake Epping, um professor de inglês comum, descobre um portal que o transporta de volta ao ano de 1958. Com a missão de impedir o assassinato do presidente John F. Kennedy, Jake precisa viver no passado por cinco anos, enfrentando desafios, paixões e as consequências imprevisíveis de mexer com a linha do tempo. Em meio a uma América nostálgica e cheia de perigos, ele se vê dividido entre salvar o futuro ou preservar o que construiu no passado.",
       anoPublicacao: "2012",
-      capa: "/images/livro1.jpg",
+      capa: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%2Fid%2FOIP.oylG3MGXh8KbqMBXcg-N2AAAAA%3Fr%3D0%26pid%3DApi&f=1&ipt=109bc2f72a234f5ac2efe92162c77d25a4900411cb841eba3a14aa3b0b6fd1e9&ipo=images",
       categoria: ["Viagem no tempo", "Passado", "Assasinato"],
       avalicao: '4',
       paginas: '400'
@@ -16,7 +16,7 @@ const livros = [
       autor: "Fiodor Dostoievski",
       sinopse: "O Príncipe Lev Míchkin retorna à Rússia após anos de tratamento para epilepsia na Suíça. Dotado de uma bondade incomum e uma visão pura do mundo, ele se vê no centro de uma sociedade aristocrática corrupta e cruel. Tentando viver com sinceridade e compaixão, Míchkin é chamado de “idiota” por aqueles ao seu redor. Envolvido em uma rede de paixões intensas, jogos de poder e tragédias humanas, ele luta para manter sua integridade em um mundo que despreza a inocência.",
       anoPublicacao: "1980",
-      capa: "/images/livro2.jpg",
+      capa: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.portugues.com.br%2F2022%2F12%2Fcapa-o-idiota.jpg&f=1&nofb=1&ipt=5e6b30ee25d23867efbb6070c1c4234974a6b58f71e51f84b88ae27e4ba9fabf",
       categoria: ["Ficcção", "Tristeza", "Inocência"],
       avalicao: '4',
       paginas: '400'
@@ -27,7 +27,7 @@ const livros = [
       autor: "Fiodor Dostoievski",
       sinopse: "Um narrador amargurado, recluso e altamente autocrítico emerge do “subsolo” da sociedade para expor suas ideias, ressentimentos e contradições. Dividido entre a lucidez extrema e a autossabotagem, ele reflete sobre a condição humana, a liberdade, o sofrimento e a hipocrisia social. Em um monólogo provocador, Dostoiévski nos convida a explorar as profundezas da mente humana — onde a razão, o orgulho e a dor coexistem em conflito constante.",
       anoPublicacao: "1969",
-      capa: "/images/livro3.jpg",
+      capa: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fhttp2.mlstatic.com%2FD_NQ_NP_944758-MLU50542272212_072022-O.webp&f=1&nofb=1&ipt=d7df326cf669fe3341b8e6a3a0d9186988caae73fc135836a6b2976f52c8d8b7",
       categoria: ["Existência", "Tristeza", "Ódio"],
       avalicao: '4',
       paginas: '400'
